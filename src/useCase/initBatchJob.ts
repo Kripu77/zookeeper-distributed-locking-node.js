0e7f665e9ed676e6ac68f959ef937f792a4b7a1c
Kripu Khadka <khadkakripu4@gmail.com>
2024-05-08 13:50:24 +1000

    into a module

--- src/useCase/initBatchJob.ts
+++ src/useCase/initBatchJob.ts
@@ -1,3 +1,32 @@
-async function setupBatchJob(docs:any){
+import { logger } from "@/helpers";
+import cron from "node-cron";
 
+const cronExpression = "*/10 * * * * *";
+
+interface Partition {
+  startIndex: number;
+  endIndex: number;
+}
+type GetPartition = () => Promise<Partition>;
+
+async function Job(getPartition: any, getDocumentsBasedOnPartition: any) {
+  const partition = await getPartition();
+  const docs = await getDocumentsBasedOnPartition(
+    partition.startIndex,
+    partition.endIndex
+  );
+  logger.info({
+    docs: docs,
+  });
+}
+
+export async function setupBatchJob(
+  getPartition: any,
+  getDocumentsBasedOnPartition: any
+) {
+  logger.info({ message: "Setting up cron event handler" });
+
+  cron.schedule(cronExpression, async () => {
+    await Job(getPartition, getDocumentsBasedOnPartition);
+  });
 }
